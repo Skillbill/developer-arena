07938342a194dbdd170fea0e10d62c3c5bc0bcff
Sergio Perticone <sergio.perticone@skillbill.it>
2018-03-09 11:07:02 +0100

    [BE] load DB config from env

--- be/config.js
+++ be/config.js
@@ -1,7 +1,6 @@
-let cache;
+let config;
 
 const defaults = {
-  //loggingLevel: 'debug',
   i18n: {
     locales: ['en', 'it'],
     defaultLocale: 'en'
@@ -15,16 +14,34 @@ const defaults = {
   }
 };
 
+const db_env_map = {
+  'host':     'DB_HOST',
+  'port':     'DB_PORT',
+  'database': 'DB_NAME',
+  'user':     'DB_USER',
+  'password': 'DB_PASSWD'
+};
+
+const init_from_env = (defs, map) => {
+  let cfg = {}
+  Object.keys(defs).forEach(k => {
+    const v = process.env[map[k]];
+    cfg[k] = (v === undefined ) ? defs[k] : v
+  });
+  return cfg
+};
+
 module.exports = {
   init : () => {
-    cache = defaults;
-    return Promise.resolve(cache);
+    config = defaults;
+    config.db = init_from_env(defaults.db, db_env_map)
+    return Promise.resolve(config);
   },
   get : () => {
-    if (!cache) {
-      throw {'msg': 'config not initialized'};
+    if (!config) {
+      throw {'error': 'config not initialized'};
     } else {
-      return cache;
+      return config;
     }
   }
 };
