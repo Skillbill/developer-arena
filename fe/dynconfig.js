4b61f55217249e870f73b5a2ea9a8c5e09044e87
Sergio <g4ll0ws@gmail.com>
2018-03-16 14:56:44 +0100

    [FE] dynconfig support for changing protocol

--- fe/dynconfig.js
+++ fe/dynconfig.js
@@ -1,25 +1,26 @@
-const fs = require('fs');
-const url = require('url');
-const configPath = 'static/configuration.js';
+const fs = require('fs')
+const url = require('url')
+const configPath = 'static/configuration.js'
 
 const panic = (str) => {
-    console.error(str);
-    process.exit(1);
+  console.error(str)
+  process.exit(1)
 }
 
 fs.readFile(configPath, (err, data) => {
+  if (err) {
+    panic(`could not load configuration file ${configPath}: ${err}`)
+  }
+  let config = eval(data.toString()) // configuration = { ... }
+  let be = url.parse(config.serverAddress)
+  be.protocol = (process.env['BE_PROTO'] || be.protocol).replace(/:$/, '')
+  be.hostname = process.env['BE_HOST'] || be.hostname
+  be.port = process.env['BE_PORT'] || be.port
+  config.serverAddress = `${be.protocol}://${be.hostname}:${be.port}`
+  console.log(config)
+  fs.writeFile(configPath, `configuration = ${JSON.stringify(config)}`, (err) => {
     if (err) {
-	panic(`could not load configuration file ${configPath}: ${err}`);
+      panic(`could not write config file ${configPath}: ${err}`)
     }
-    let config = eval(data.toString()); // configuration = { ... }
-    let be = url.parse(config.serverAddress);
-    be.host = process.env['BE_HOST'] || be.host;
-    be.port = process.env['BE_PORT'] || be.bePort || 3000;
-    config.serverAddress = `${be.protocol}//${be.host}:${be.port}`;
-    console.log(config);
-    fs.writeFile(configPath, `configuration = ${JSON.stringify(config)}`, (err) => {
-	if (err) {
-	    panic(`could not write config file ${configPath}: ${err}`);
-	}
-    });
-});
+  })
+})
