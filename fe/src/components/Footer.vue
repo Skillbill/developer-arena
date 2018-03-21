bbe99e6d07977406b25740c86cd34e94bbc3ac2f
Antonio Salvati <antonio.salvati@skillbill.it>
2018-03-21 12:39:21 +0100

    [FE] fixed footer logo image

--- fe/src/components/Footer.vue
+++ fe/src/components/Footer.vue
@@ -13,7 +13,7 @@
     </div>
     <div class="footer-info">
       <p>Skillbill Developer Arena Contest <br> &copy; <a href="http://www.skillbill.it/">Skillbill</a> 2018</p>
-      <img class="logo" src="assets/logo-skillbill.png" alt="Logo Skillbill">
+      <img class="logo" src="static/graphics/assets/logo-skillbill.png" alt="Logo Skillbill">
     </div>
   </footer>
 </template>
