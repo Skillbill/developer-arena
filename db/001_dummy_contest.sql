abc54dfa98ee9d4f62f74804a618a5cdab2de24f
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2018-03-26 17:53:25 +0200

    descrizione italiana e rules primo contest

--- db/001_dummy_contest.sql
+++ db/001_dummy_contest.sql
@@ -55,13 +55,16 @@ INSERT INTO "public" . contest_i18n(
     1,
     'description',
     'it',
-    'Il tema di questa gara è lo sviluppo di un programma HTML5 animato e/o interattivo ispirato al mondo **Marvel** più specificatamente al nuovo film in arrivo “Avengers Infinity War”. Per esempio potrai sviluppare una animazione con i personaggi Marvel, la visualizzazione di un algoritmo genetico per pilotare i proiettili sparati da Iron Man, la trasformazione di Banner in HULK basata sulla velocità dei click del mouse, prendere tutte le gemme dell’Infinito giocando un rhythm game… le idee possono essere davvero tante e il limite è quindi solo la tua immaginazione.
-Potrai usare qualsiasi tecnica web.
+    'Il tema di questa gara è lo sviluppo di un programma **HTML5** animato e/o interattivo ispirato al mondo **Marvel** più specificatamente al nuovo film in arrivo **“Avengers Infinity War”**.
+Ecco alcuni esempi :
 
-Il tuo programma verrà aperto nel browser Chrome ‘Official Build’ 64 bit versione >= 65 in una macchina virtuale avente 8GB di RAM, 4 core, impostazione di rete offline e con sistema operativo Xubuntu appena installato (qui… c’è export VirtualBox della macchina) .
-Solo i primi 15 classificati nella votazione online che partirà il 23/04/2018 potranno presentare dal vivo i loro progetti nell’evento che avverrà il 14/05/2018 e verranno giudicati da una giuria di **tecnici esperti**. A fine evento i primi tre classificati vinceranno dei premi !
-Per maggiori dettagli leggi il regolamento generale che trovi qui...
-'
+ - una animazione ispirata al film con i vari personaggi
+ - la visualizzazione di un algoritmo genetico per pilotare i proiettili sparati da Iron Man
+ - la trasformazione di Banner in HULK basata sulla velocità dei click del mouse
+ - prendere tutte le gemme dell’Infinito giocando un rhythm game
+
+le idee possono essere davvero tante e il limite è quindi solo la tua immaginazione.
+Potrai usare qualsiasi tecnica web.'
 );
 
 INSERT INTO "public" . contest_i18n(
@@ -73,9 +76,8 @@ INSERT INTO "public" . contest_i18n(
     1,
     'rules',
     'it',
-    '1. Regola numero 1.
-2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa labore accusamus commodi sequi natus obcaecati odio deleniti praesentium sed quam, modi pariatur soluta nihil autem nisi numquam ea animi iure.
-3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa labore accusamus commodi sequi natus obcaecati odio deleniti praesentium sed quam, modi pariatur soluta nihil autem nisi numquam ea animi iure.
+    '1. Il tuo programma verrà aperto nel browser **Chrome** ‘Official Build’ 64 bit versione >= 65
+2. Il browser sarà ospitato in una macchina virtuale avente **8GB** di RAM, **4 core**, impostazione di rete **offline** e con sistema operativo **Xubuntu** 16.04.x appena installato (qui… c’è export VirtualBox della macchina)
 '
 );
 
