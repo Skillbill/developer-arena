8fc9a29bdb960c0e79ec34bc4fc1df9473c9de59
Antonio Salvati <antonio.salvati@skillbill.it>
2018-03-26 17:47:06 +0200

    [FE] home improvements

--- fe/src/components/Home.vue
+++ fe/src/components/Home.vue
@@ -1,37 +1,39 @@
 <template>
   <main class="home">
     <section>
-      <h2 class="main-info" v-if="contest">
-        {{$t("contestBanner.contest")}} <strong>#{{contest.id}}</strong>
-        <template v-if="contest.state === 'PRESENTATION'">
-          {{$t("contestBanner.presentation")}}
-          <Countdown :date="this.contest.endPresentation"></Countdown>
-        </template>
-        <template v-if="contest.state === 'APPLYING'">
-          {{$t("contestBanner.applying")}}
-        </template>
-        <template v-if="contest.state === 'VOTING'">
-          {{$t("contestBanner.voting")}}
-        </template>
-        <template v-if="contest.state === 'CLOSED'">
-          {{$t("contestBanner.closed")}}
-        </template>
-        <template v-if="contest.state === 'PAST'">
-          {{$t("contestBanner.past")}}
-        </template>
-      </h2>
-      <div class="contest" v-if="contest">
-        <h2>{{contest.title}}</h2>
-        <div v-md="contest.description"></div>
-        <h3>{{$t('rules')}}</h3>
-        <div v-md="contest.rules"></div>
-        <template v-if="contest.state === 'APPLYING'">
-          <form action="#" class="submit-project" v-on:submit.prevent="applyContest">
-            <button type="submit">{{ $t('applyContest') }}</button>
-          </form>
-        </template>
-      </div>
-      <div v-else class="progress"></div>
+      <div v-if="loading" class="progress" key="progress"></div>
+      <template v-else>
+        <h2 class="main-info" v-if="contest">
+          {{$t("contestBanner.contest")}} <strong>#{{contest.id}}</strong>
+          <template v-if="contest.state === 'PRESENTATION'">
+            {{$t("contestBanner.presentation")}}
+            <Countdown :date="this.contest.endPresentation"></Countdown>
+          </template>
+          <template v-if="contest.state === 'APPLYING'">
+            {{$t("contestBanner.applying")}}
+          </template>
+          <template v-if="contest.state === 'VOTING'">
+            {{$t("contestBanner.voting")}}
+          </template>
+          <template v-if="contest.state === 'CLOSED'">
+            {{$t("contestBanner.closed")}}
+          </template>
+          <template v-if="contest.state === 'PAST'">
+            {{$t("contestBanner.past")}}
+          </template>
+        </h2>
+        <div class="contest" v-if="contest">
+          <h2>{{contest.title}}</h2>
+          <div v-md="contest.description"></div>
+          <h3>{{$t('rules')}}</h3>
+          <div v-md="contest.rules"></div>
+          <template v-if="contest.state === 'APPLYING'">
+            <form action="#" class="submit-project" v-on:submit.prevent="applyContest">
+              <button type="submit">{{ $t('applyContest') }}</button>
+            </form>
+          </template>
+        </div>
+      </template>
     </section>
   </main>
 </template>
@@ -40,13 +42,21 @@
 import Countdown from '@/components/Countdown'
 
 export default {
+  data() {
+    return {
+      loading: false
+    }
+  },
   computed: {
     contest () {
       return this.$store.state.contest;
     }
   },
   created () {
-    this.$store.dispatch('loadLastContest');
+    this.loading = true;
+    this.$store.dispatch('loadLastContest').then(() => {
+      this.loading = false;
+    });
   },
   components: {
     Countdown
