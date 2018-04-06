0ab03d334dd99e4162305a505482ee00ee8c0450
Antonio Salvati <antonio.salvati@skillbill.it>
2018-04-06 09:59:46 +0200

    [FE] removed 'clip-path' and 'nowrap' from markdown generated content

--- fe/src/directives/markdown.js
+++ fe/src/directives/markdown.js
@@ -3,6 +3,7 @@ import marked from 'marked'
 function md(el, binding, vnode) {
   let html = marked(binding.value || '', {sanitize: true});
   el.innerHTML = html;
+  el.classList.add('v-md');
 }
 
 export default {
