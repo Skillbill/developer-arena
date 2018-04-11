88f86c94c5f3197a511c1b252dfa0f1a0da18c3b
antonyme <guillaumeduliscouet@gmail.com>
2018-04-11 11:45:03 +0200

    [BO] patch contest date request

--- bo/src/components/EditDates.vue
+++ bo/src/components/EditDates.vue
@@ -4,14 +4,14 @@
     <form v-on:submit.prevent="saveNewDates">
       <div class="d-flex flex-column flex-md-row">
         <div class="form-group px-2">
-          <label for="date0">End applying:</label>
-          <input type="date" required="true" class="form-control date" id="date0"
-            :value="contestDate0" @input="updateDate(0, $event)">
-        </div>
-        <div class="form-group px-2">
           <label for="date1">End presentation: </label>
           <input type="date" required="true" class="form-control date" id="date1"
             :value="contestDate1" @input="updateDate(1, $event)">
+        </div>
+        <div class="form-group px-2">
+          <label for="date0">End applying:</label>
+          <input type="date" required="true" class="form-control date" id="date0"
+            :value="contestDate0" @input="updateDate(0, $event)">
         </div>
         <div class="form-group px-2">
           <label for="date2">End voting: </label>
@@ -27,6 +27,8 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import axios from 'axios'
+import * as utils from '../utils'
 
 export default {
   name: 'EditDates',
@@ -36,6 +38,8 @@ export default {
   },
   computed: {
     ...mapGetters({
+      user: 'getUser',
+      contest: 'getContest',
       contestDate0: 'getContestDate0',
       contestDate1: 'getContestDate1',
       contestDate2: 'getContestDate2'
@@ -47,8 +51,26 @@ export default {
         this.$store.commit('setContestDate'.concat(index), e.target.value)
       }
     },
-    saveNewDates () {
-      // TODO save dates in DB
+    async saveNewDates () {
+      var headers = {
+        'Authorization': '12345',
+        'Content-Type': 'application/json',
+        'Accept-Language': 'en'
+      }
+      axios({
+        method: 'patch',
+        url: utils.getApiUrl('/contest/' + this.contest.id),
+        data: {
+          endPresentation: this.contest.endPresentation,
+          endApplying: this.contest.endApplying,
+          endVoting: this.contest.endVoting
+        },
+        headers
+      }).then(response => {
+        console.log(response.data)
+      }).catch(e => {
+        console.error(e)
+      })
     }
   },
   created () {
