c88ee496a3794294c30dc939a48397155f14dbd9
Sergio Perticone <sergio.perticone@skillbill.it>
2018-04-20 11:22:05 +0200

    [BE] admin can dis-approve a project

--- be/routers/admin/project.js
+++ be/routers/admin/project.js
@@ -5,15 +5,16 @@ const express = require('express')
 const router = express.Router({mergeParams: true})
 
 router.put('/:projectId/approve', approveProject)
+router.delete('/:projectId/approve', approveProject)
 
 function approveProject(req, res, next) {
     const id = req.params.projectId
-    const value = (req.body && req.body.approved == false) ? false : true
+    const value = (req.method == 'PUT') ? true : false
     persistence.getProjectById(id).then(project => {
         if (!project) {
             return next(error.projectNotFound)
         }
-        return project.approved == value ? Promise.resolve() : persistence.setProjectApproved(id, value)
+        return persistence.setProjectApproved(id, value)
     }).then(() => {
         res.status(http.noContent).send()
     }).catch(err => {
