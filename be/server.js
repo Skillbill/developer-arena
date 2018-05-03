2f7880f797c7c5b598eb8e00a0bbfed7529b2967
Sergio Perticone <sergio.perticone@skillbill.it>
2018-05-03 17:08:56 +0200

    [BE] made server.js more test-friendly

--- be/server.js
+++ be/server.js
@@ -2,7 +2,6 @@ const logger = require('@/lib/logger')
 const libconfig = require('@/lib/config')
 const auth = require('@/lib/auth')
 
-const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const i18n = require('i18n')
@@ -12,12 +11,11 @@ const cors = require('cors')
 logger.info('starting server')
 
 try {
-    const cfgpath = process.env.CONFIG || path.join(__dirname, './config.json')
-    const config = libconfig.init(path.resolve(cfgpath))
+    const cfgpath = process.env.CONFIG || require.resolve('@/config.json')
+    const config = libconfig.init(cfgpath)
 
     auth.init(config)
     const app = express()
-    logger.info('starting api')
     i18n.configure(Object.assign({
         queryParameter: 'lang',
         directory: __dirname + '/locales',
@@ -29,30 +27,29 @@ try {
     app.use(i18n.init)
     app.use(fileUpload())
     app.use(cors())
+
     if (config.server.logger) {
         app.use(require('./routers/middleware/logger'))
     }
     app.use('/info', require('./routers/info'))
     app.use('/1.0', require('./routers'))
+    module.exports = app
 
-    const port = process.env.BE_PORT || 3000
-    app.listen(port, () => {
-        logger.log(`server listening on ${port}`)
-    })
+    const port = config.server.port
+    if (port) {
+        app.listen(port, () => {
+            logger.log(`server listening on ${port}`)
+        })
+    }
 } catch (error) {
     logger.error('could not start the server:', error)
 }
 
 if (process.pid == 1) { // docker
-    // allow ^C on a foreground instance to stop the container
-    process.on('SIGINT', () => {
-        logger.log('got SIGINT: exit')
-        process.exit()
-    })
-
-    // promptly shutdown on docker stop */
-    process.on('SIGTERM', () => {
-        logger.log('got SIGTERM: exit')
-        process.exit()
-    })
+    for (const signal of ['SIGINT', 'SIGTERM']) {
+        process.on(signal, () => {
+            logger.log(`got ${signal}: exit`)
+            process.exit()
+        })
+    }
 }
