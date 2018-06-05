326d58a0cb14140141fffed70df99a8079c23205
antonyme <guillaumeduliscouet@gmail.com>
2018-06-05 17:50:11 +0200

    [BO] get config from confit

--- bo/src/main.js
+++ bo/src/main.js
@@ -4,12 +4,12 @@ import router from './lib/router'
 import store from './lib/store'
 import auth from './lib/auth'
 import api from './lib/api'
+import Confit from './lib/confit'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Toastr from 'toastr'
 import 'toastr/build/toastr.min.css'
-import axios from 'axios'
 import VueLogger from 'vuejs-logger'
 
 Vue.config.productionTip = false
@@ -18,18 +18,16 @@ Vue.use(BootstrapVue)
 Vue.use(VueLogger, {logLevel: 'info', showConsoleColors: true, showLogLevel: true})
 Vue.$toastr = Vue.prototype.$toastr = Toastr
 
-let configurationPath = location.origin + location.pathname + 'static/configuration.json'
 Vue.$log.info(`Protocol: ${location.protocol}, Host: ${location.hostname}, Path: ${location.pathname}, Port: ${location.port}`)
-Vue.$log.info(`configuration loaded from: ${configurationPath}`)
 
-axios({
-  method: 'get',
-  url: configurationPath
-}).then(rep => {
-  let config = rep.data
+new Confit({
+  repoId: 'f7ca505d-a32f-4909-aab3-a28b4f1c4ee8'
+}).getConf(location.hostname + '-bo', {
+  alias: true
+}).then(config => {
+  Vue.$log.info('Retreived config from confit:', config)
   config.statRes = location.origin + location.pathname + 'static'
   Vue.$config = Vue.prototype.$config = config
-  if (typeof Vue.$config.firebase.devMode !== 'boolean') Vue.$log.error('devMode in configuration.json should be a boolean')
   auth.init(config, showApp)
   api.init(config)
 }).catch(e => {
