9a8bd086c62587492d5062209a65a97fac652742
Sergio Perticone <sergio.perticone@skillbill.it>
2018-06-07 11:12:50 +0200

    [BE] added confit support

--- be/lib/config.js
+++ be/lib/config.js
@@ -1,16 +1,50 @@
 const logger = require('@/lib/logger')
 const fs = require('fs')
+
 let config = {}
 
-module.exports = {
-    init: (filePath) => {
-        logger.log(`initializing config from ${filePath}`)
-        try {
-            config = JSON.parse(fs.readFileSync(filePath))
-        } catch (err) {
-            throw `${filePath}: ${err}`
+const initFromPath = (filePath) => new Promise((resolve, reject) => {
+    logger.log(`initializing config from ${filePath}`)
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            return reject(err)
         }
-        return config
-    },
+        try {
+            config = JSON.parse(data)
+            resolve(config)
+        } catch (err) {
+            reject(`${filePath}: ${err}`)
+        }
+    })
+})
+
+const initWithConfit = (client) => new Promise((resolve, reject) => {
+    const env = process.env.NODE_ENV
+    if (!env) {
+        return reject('confit error: NODE_ENV not set')
+    }
+    const path = `/${env}/be.json`
+    logger.log(`initializing with Confit from ${path}`)
+    client.getConf(path).then(obj => {
+        config = obj
+        resolve(config)
+    }).catch(reject)
+})
+
+function init(resource) {
+    switch (typeof resource) {
+    case 'string':
+        return initFromPath(resource)
+
+    case 'object':
+        return initWithConfit(resource)
+
+    default:
+        return Promise.reject('unexpected resource type: ' + typeof resource)
+    }
+}
+
+module.exports = {
+    init,
     get: () => Object.assign({}, config)
 }
--- be/server.js
+++ be/server.js
@@ -7,49 +7,55 @@ const bodyParser = require('body-parser')
 const i18n = require('i18n')
 const fileUpload = require('express-fileupload')
 const cors = require('cors')
+const Confit = require('confit-client')
 
-logger.info('starting server')
+async function init(app) {
+    logger.log('starting server')
+    try {
+        const cfgpath = process.env.CONFIG || require.resolve('@/config.json')
+        let confit
+        if (process.env.CONFIT_TOKEN && process.env.CONFIT_REPOID) {
+            confit = new Confit()
+            logger.info(`confit repoId: ${confit.repoId}`)
+        }
+        const config = await libconfig.init(confit || cfgpath)
+        auth.init(config)
+        i18n.configure(Object.assign({
+            queryParameter: 'lang',
+            directory: __dirname + '/locales',
+            updateFiles: false
+        }, config.i18n))
+        app.disable('x-powered-by')
+        app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(i18n.init)
+        app.use(fileUpload())
+        app.use(cors())
 
-try {
-    const cfgpath = process.env.CONFIG || require.resolve('@/config.json')
-    const config = libconfig.init(cfgpath)
-
-    auth.init(config)
-    const app = express()
-    i18n.configure(Object.assign({
-        queryParameter: 'lang',
-        directory: __dirname + '/locales',
-        updateFiles: false
-    }, config.i18n))
-    app.disable('x-powered-by')
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({extended: false}))
-    app.use(i18n.init)
-    app.use(fileUpload())
-    app.use(cors())
-
-    if (config.server.logger) {
-        app.use(require('./routers/middleware/logger'))
-    }
-    app.use('/info', require('./routers/info'))
-    app.use('/1.0', require('./routers'))
-    module.exports = app
-
-    const port = config.server.port
-    if (port) {
-        app.listen(port, () => {
-            logger.log(`server listening on ${port}`)
-        })
-    }
-} catch (error) {
-    logger.error('could not start the server:', error)
-}
-
-if (process.pid == 1) { // docker
-    for (const signal of ['SIGINT', 'SIGTERM']) {
-        process.on(signal, () => {
-            logger.log(`got ${signal}: exit`)
-            process.exit()
-        })
+        if (config.server.logger) {
+            app.use(require('./routers/middleware/logger'))
+        }
+        app.use('/info', require('./routers/info'))
+        app.use('/1.0', require('./routers'))
+        const port = config.server.port
+        if (port) {
+            app.listen(port, () => {
+                logger.log(`server listening on ${port}`)
+            })
+        }
+    } catch (error) {
+        logger.error('could not start the server:', error)
     }
 }
+
+for (const signal of ['SIGINT', 'SIGTERM']) {
+    process.on(signal, () => {
+        logger.log(`got ${signal}: exit`)
+        process.exit()
+    })
+}
+
+const app = express()
+init(app)
+
+module.exports = app
