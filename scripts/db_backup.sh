90efefa287d503dd7f940a2c1673e82b91498591
Sergio Perticone <sergio.perticone@skillbill.it>
2018-06-11 10:27:58 +0200

    made db_backup.sh play nice with logrotate

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -6,9 +6,9 @@ user=${DBUSER:-'postgres'}
 database=${DATABASE:-'developer-arena'}
 bckdir=${BACKUPDIR:-/var/backups}
 
-ts=`date -u +%Y%m%d_%H%M%S`
+ts=`date -u +%s`
 docker_id=`docker ps | grep developer-arena-db | awk '{print $1}'`
-backup="${bckdir}/${database}_${ts}.sql.gz"
+backup="${bckdir}/${database}.sql.gz"
 
 docker exec $docker_id \
-       pg_dumpall -c --inserts -U $user -l $database | gzip > $backup && echo created $backup
+       pg_dumpall -c --inserts -U $user -l $database | gzip > $backup.${ts} && ln -f ${backup}.${ts} ${backup}
