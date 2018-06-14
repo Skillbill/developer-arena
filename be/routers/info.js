42b41b0ce7803c967f5cabb507b6aedb6cc1275b
Sergio Perticone <sergio.perticone@skillbill.it>
2018-06-14 18:07:57 +0200

    made eslint happy again

--- be/routers/info.js
+++ be/routers/info.js
@@ -4,7 +4,7 @@ const http = require('@/lib/http')
 const version = require('../version.json')
 
 router.get('/', (req, res) => {
-    res.status(http.ok).send(version);
+    res.status(http.ok).send(version)
 })
 
 module.exports = router
