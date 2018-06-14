7f05fb6f3ca3086a74801789e9ce96117d8b149d
Sergio Perticone <sergio.perticone@skillbill.it>
2018-06-14 10:03:35 +0200

    [BE][DB] judge model refactoring

--- be/model/judge_image.js
+++ be/model/judge_image.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 
 module.exports = {
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+    judgeId : {type: Sequelize.INTEGER, field: 'judge_id'},
     mimetype: {type: Sequelize.STRING, field: 'mimetype' },
     mtime: {type: Sequelize.DATE, field: 'mtime' },
     name: {type: Sequelize.STRING, field: 'name'},
