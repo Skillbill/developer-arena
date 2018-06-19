b79bfaf062ff06cbd2a1551cfa7b55a68c1feb26
Sergio Perticone <sergio.perticone@skillbill.it>
2018-06-19 11:36:51 +0200

    [FE] locales: s/Regolamento/Regole/

--- fe/src/i18n/locales/it.js
+++ fe/src/i18n/locales/it.js
@@ -4,7 +4,7 @@ module.exports = {
     home: 'Home',
     projects: 'Progetti',
     jury: 'Giuria',
-    rules: 'Regolamento',
+    rules: 'Regole',
     signIn: 'Accedi',
     signOut: 'Esci'
   },
